74773a27cfe532643a796b306f82288285f07602
Syfaro <syfaro@foxpaw.in>
2016-04-02 11:09:12 -0500

    Consistency with types of quotes used in own ext.

--- ext/client_cert_auth/main.php
+++ ext/client_cert_auth/main.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Name: Client Certificate Auth
  * Author: Syfaro <syfaro@foxpaw.in>
@@ -9,18 +10,18 @@
 class Update extends Extension {
 	public function onInitExt(InitExtEvent $event) {
 		global $config;
-		$config->set_default_bool("enable_cert_auth", false);
+		$config->set_default_bool('enable_cert_auth', false);
 	}
 
 	public function onSetupBuilding(SetupBuildingEvent $event) {
-		$sb = new SetupBlock("Client Certificate Authentication");
-		$sb->add_bool_option("enable_cert_auth", "Enable: ");
+		$sb = new SetupBlock('Client Certificate Authentication');
+		$sb->add_bool_option('enable_cert_auth', 'Enable: ');
 		$event->panel->add_block($sb);
 	}
 
 	public function onAdminBuilding(AdminBuildingEvent $event) {
 		global $config;
-		if($config->get_string('transload_engine') !== "none"){
+		if($config->get_string('transload_engine') !== 'none'){
 			$this->theme->display_admin_block();
 		}
 	}
